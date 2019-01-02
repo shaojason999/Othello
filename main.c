d52f5f381717a41ca4f7392bbb64db3e5a7e2ee7
shaojason999 <shaojason999@gmail.com>
2019-01-02 21:17:56 +0800

    Update main.c

--- main.c
+++ main.c
@@ -8,34 +8,15 @@ int gameboard[9][9];	//use gameboard[1][1]~gameboard[8][8]
 int hash[65536];
 int move;
 unsigned long long int board[3];
+
+extern int check_legal(int row, int column, int color);
+extern void init_hash_table();
+
 unsigned int hash_function(unsigned long long int board)
 {
 	board=(board>>32)^(board&0xffffffff);
 	board=(board>>16)^(board&0xffff);
 	return (unsigned int)board;
-}
-void init_hash_table()
-{
-	unsigned int index;
-	index=hash_function(0x0000001008000000+0x0000000810000000);	//white+black
-	hash[index]=(3<<4)+4;
-	index=hash_function(0x0000000008000000+0x0000101810000000);
-	hash[index]=(5<<4)+3;
-	index=hash_function(0x0000000008000000+0x0000003810000000);
-	hash[index]=(3<<4)+5;
-	index=hash_function(0x0000001000000000+0x0000000818080000);
-	hash[index]=(4<<4)+6;
-	index=hash_function(0x0000001000000000+0x000000081c000000);
-	hash[index]=(6<<4)+6;
-/*
-	index=hash_function(
-	hash[index]=
-
-	index=hash_function(0x0000000038000000+0x0000101800000000);
-	hash[index]=6<<4+5;
-	index=hash_function(0x0000000030000000+0x0000101808080000);
-	hash[index]=2<<4+4;
-*/
 }
 int count_color(int color)
 {
@@ -58,121 +39,6 @@ void show_gameboard()
 			printf("%d ",gameboard[i][j]);
 		printf("\n");
 	}
-}
-int check_legal(int row, int column, int color)
-{
-	int legal=0;
-	int opponent_color;
-	int i,j;
-	unsigned long long int temp=1;
-	if(color==1)
-		opponent_color=2;
-	else
-		opponent_color=1;
-	/*check empty*/
-	if(gameboard[row][column]!=0)
-		return legal;
-	/*check up*/
-	for(i=row-1;i>0;--i)
-		if(gameboard[i][column]!=opponent_color)
-			if(gameboard[i][column]==color && i!=(row-1)){
-				legal=1;
-				for(j=i+1;j<row;++j){
-					gameboard[j][column]=color;
-					board[color]|=temp<<(((8-j)<<3)+(8-column));
-					board[opponent_color]&=~(temp<<(((8-j)<<3)+(8-column)));
-				}
-			}else
-				break;
-	/*check down*/
-	for(i=row+1;i<=8;++i)
-		if(gameboard[i][column]!=opponent_color)
-			if(gameboard[i][column]==color && i!=(row+1)){
-				legal=1;
-				for(j=i-1;j>row;--j){
-					gameboard[j][column]=color;
-					board[color]|=temp<<(((8-j)<<3)+(8-column));
-					board[opponent_color]&=~(temp<<(((8-j)<<3)+(8-column)));
-				}
-			}else
-				break;
-	/*check right*/
-	for(i=column+1;i<=8;++i)
-		if(gameboard[row][i]!=opponent_color)
-			if(gameboard[row][i]==color && i!=(column+1)){
-				legal=1;
-				for(j=i-1;j>column;--j){
-					gameboard[row][j]=color;
-					board[color]|=temp<<(((8-row)<<3)+(8-j));
-					board[opponent_color]&=~(temp<<(((8-row)<<3)+(8-j)));
-				}
-			}else
-				break;
-	/*check left*/
-	for(i=column-1;i>0;--i)
-		if(gameboard[row][i]!=opponent_color)
-			if(gameboard[row][i]==color && i!=(column-1)){
-				legal=1;
-				for(j=i+1;j<column;++j){
-					gameboard[row][j]=color;
-					board[color]|=temp<<(((8-row)<<3)+(8-j));
-					board[opponent_color]&=~(temp<<(((8-row)<<3)+(8-j)));
-				}
-			}else
-				break;
-	/*check upper right*/
-	for(i=1;(row-i)>0 && (column+i)<=8;++i)
-		if(gameboard[row-i][column+i]!=opponent_color)
-			if(gameboard[row-i][column+i]==color && i!=1){
-				legal=1;
-				for(j=i-1;j>0;--j){
-					gameboard[row-j][column+j]=color;
-					board[color]|=temp<<(((8-(row-j))<<3)+(8-(column+j)));
-					board[opponent_color]&=~(temp<<(((8-(row-j))<<3)+(8-(column+j))));
-				}
-			}else
-				break;
-	/*check bottom left*/
-	for(i=1;(row+i)<=8 && (column-i)>0;++i)
-		if(gameboard[row+i][column-i]!=opponent_color)
-			if(gameboard[row+i][column-i]==color && i!=1){
-				legal=1;
-				for(j=i-1;j>0;--j){
-					gameboard[row+j][column-j]=color;
-					board[color]|=temp<<(((8-(row+j))<<3)+(8-(column-j)));
-					board[opponent_color]&=~(temp<<(((8-(row+j))<<3)+(8-(column-j))));
-				}
-			}else
-				break;
-	/*check upper left*/
-	for(i=1;(row-i)>0 && (column-i)>0;++i)
-		if(gameboard[row-i][column-i]!=opponent_color)
-			if(gameboard[row-i][column-i]==color && i!=1){
-				legal=1;
-				for(j=i-1;j>0;--j){
-					gameboard[row-j][column-j]=color;
-					board[color]|=temp<<(((8-(row-j))<<3)+(8-(column-j)));
-					board[opponent_color]&=~(temp<<(((8-(row-j))<<3)+(8-(column-j))));
-				}
-			}else
-				break;
-	/*chech bottom right*/
-	for(i=1;(row+i)<=8 && (column+i)<=8;++i)
-		if(gameboard[row+i][column+i]!=opponent_color)
-			if(gameboard[row+i][column+i]==color && i!=1){
-				legal=1;
-				for(j=i-1;j>0;--j){
-					gameboard[row+j][column+j]=color;
-					board[color]|=temp<<(((8-(row+j))<<3)+(8-(column+j)));
-					board[opponent_color]&=~(temp<<(((8-(row+j))<<3)+(8-(column+j))));
-				}
-			}else
-				break;
-	if(legal){
-		gameboard[row][column]=color;
-		board[color]|=temp<<(((8-row)<<3)+(8-column));
-	}
-	return legal;
 }
 void init()
 {
